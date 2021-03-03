49a702c87fc129b1bbcb0247ba707be308f0d10d
unknown <beatamaro@interia.pl>
2021-03-03 21:27:51 +0100

    secrets

--- public/javascripts/gsap/gsap-campgrounds.js
+++ public/javascripts/gsap/gsap-campgrounds.js
@@ -1,4 +1,4 @@
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
 const tlCampgrounds = gsap.timeline({
   defaults: {
@@ -11,3 +11,18 @@ tlCampgrounds.fromTo(
   { opacity: 0, y: "-100vh", scale: 2 },
   { opacity: 1, y: 0, scale: 1 }
 );
+
+gsap.from(".card", {
+  scrollTrigger: {
+    trigger: ".pagination",
+    start: "top center",
+    toggleActions: "play pause pause pause",
+    markers: {
+      startColor: "blue",
+    },
+  },
+  x: "90vw",
+  stagger: 0.5,
+  duration: 1,
+  ease: "elastic",
+});
